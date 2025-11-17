808346a8322489825630c199eb5cf0009c345616
RichardT1996 <richard.thomas1996@outlook.com>
2025-11-17 15:53:06 +0000

    change name

--- site/index.php
+++ site/index.php
@@ -11,14 +11,12 @@
 
 <div class="container" id="main-content">
 	<h2>Welcome to my website!</h2>
-	<p>hello</p>
+	<h3>My name is Richard Thomas</h3>
 
 	<p>
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-	</p>
-	<p>
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-	</p>
+        Welcome to my website.
+    </p>
+	
 </div>
 
 <h1>hello matey</h1>
